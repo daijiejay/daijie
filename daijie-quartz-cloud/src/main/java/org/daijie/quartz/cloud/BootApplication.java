4785875cc0a94d15ec9d38aef6dbcb7286cd3622
daijie <daijie@ip-192-169-244-167.ip.secureserver.net>
2017-08-12 11:17:16 +0800

    加入定时任务

---
+++ daijie-quartz-cloud/src/main/java/org/daijie/quartz/cloud/BootApplication.java
@@ -0,0 +1,20 @@
+package org.daijie.quartz.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableEurekaClient
+@EnableScheduling
+public class BootApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(BootApplication.class).web(true).run(args);
+	}
+}
