b160ad06e96df81eb490049786b68faecb7deefa
daijie <daijie_jay@126.com>
2017-10-16 21:30:41 +0800

    整合activiti

---
+++ daijie-activiti-cloud/src/main/java/org/daijie/activiti/cloud/BootApplication.java
@@ -0,0 +1,14 @@
+package org.daijie.activiti.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BootApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(BootApplication.class).web(true).run(args);
+	}
+}
