1bb68ac561e2520a7776c8e045abb0bb5850444b
daijie <daijie_jay@126.com>
2017-11-23 10:29:37 +0800

    优化代码

--- daijie-mybatis-model/src/main/java/org/daijie/mybatis/model/User.java
+++ daijie-mybatis-model/src/main/java/org/daijie/mybatis/model/User.java
@@ -4,70 +4,98 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 public class User implements Serializable {
+    /**
+     * 用户ID
+     */
     @Id
     @Column(name = "user_id")
     private Integer userId;
 
+    /**
+     * 用户名
+     */
     @Column(name = "user_name")
     private String userName;
 
+    /**
+     * 密码
+     */
     private String password;
 
+    /**
+     * 盐
+     */
     private String salt;
 
     private static final long serialVersionUID = 1L;
 
     /**
-     * @return user_id
+     * 获取用户ID
+     *
+     * @return user_id - 用户ID
      */
     public Integer getUserId() {
         return userId;
     }
 
     /**
-     * @param userId
+     * 设置用户ID
+     *
+     * @param userId 用户ID
      */
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
 
     /**
-     * @return user_name
+     * 获取用户名
+     *
+     * @return user_name - 用户名
      */
     public String getUserName() {
         return userName;
     }
 
     /**
-     * @param userName
+     * 设置用户名
+     *
+     * @param userName 用户名
      */
     public void setUserName(String userName) {
         this.userName = userName == null ? null : userName.trim();
     }
 
     /**
-     * @return password
+     * 获取密码
+     *
+     * @return password - 密码
      */
     public String getPassword() {
         return password;
     }
 
     /**
-     * @param password
+     * 设置密码
+     *
+     * @param password 密码
      */
     public void setPassword(String password) {
         this.password = password == null ? null : password.trim();
     }
 
     /**
-     * @return salt
+     * 获取盐
+     *
+     * @return salt - 盐
      */
     public String getSalt() {
         return salt;
     }
 
     /**
-     * @param salt
+     * 设置盐
+     *
+     * @param salt 盐
      */
     public void setSalt(String salt) {
         this.salt = salt == null ? null : salt.trim();
