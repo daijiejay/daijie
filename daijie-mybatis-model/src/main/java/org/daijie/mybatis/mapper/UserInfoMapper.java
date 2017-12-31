79317432a876ddcc2069ee668062f370021afbec
daijie <daijie_jay@126.com>
2017-12-31 22:15:15 +0800

    迁出案例项目，解决jar包冲突

--- daijie-mybatis-model/src/main/java/org/daijie/mybatis/mapper/UserInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package org.daijie.mybatis.mapper;
-
-import org.daijie.mybatis.model.UserInfo;
-import tk.mybatis.mapper.common.ConditionMapper;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-public interface UserInfoMapper extends Mapper<UserInfo>, ConditionMapper<UserInfo>, MySqlMapper<UserInfo> {
-}
--- daijie-mybatis-model/src/main/java/org/daijie/mybatis/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package org.daijie.mybatis.mapper;
-
-import org.daijie.mybatis.model.User;
-import tk.mybatis.mapper.common.ConditionMapper;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-public interface UserMapper extends Mapper<User>, ConditionMapper<User>, MySqlMapper<User> {
-}
