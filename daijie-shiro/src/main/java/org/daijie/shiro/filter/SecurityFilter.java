331865297505170e65fce7f34b1d6e342aac1d79
daijie <daijie@caihangjia.com>
2018-06-06 14:45:19 +0800

    shiro权限拦截器优化

--- daijie-shiro/src/main/java/org/daijie/shiro/filter/SecurityFilter.java
+++ daijie-shiro/src/main/java/org/daijie/shiro/filter/SecurityFilter.java
@@ -1,9 +1,15 @@
 package org.daijie.shiro.filter;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 import org.apache.shiro.web.filter.PathMatchingFilter;
+import org.daijie.core.controller.enums.ResultCode;
+import org.daijie.core.result.ApiResult;
+import org.daijie.core.result.factory.ModelResultInitialFactory.Result;
 import org.daijie.shiro.session.ShiroRedisSession.Redis;
 
 /**
@@ -17,6 +23,19 @@ public class SecurityFilter extends PathMatchingFilter {
 	@Override
 	public boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
 		if(Redis.getSession() == null){
+			PrintWriter out = null;
+			try{
+				response.setContentType("application/json;charset=utf-8");
+				response.setCharacterEncoding("UTF-8");
+				out = response.getWriter();
+				out.write(Result.build(null, ResultCode.CODE_400.getDescription(), ApiResult.ERROR, ResultCode.CODE_400).toJsonStr());
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (out != null) {
+					out.close();
+				}
+			}
 			return false;
 		}
 		return true;
