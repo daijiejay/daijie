1fc55ff105840e50c90df9347133fbf5ac44d7bc
jerry.dai <jerry.dai@shijigroup.com>
2020-02-25 15:35:29 +0800

    解决多个mapper注册失败

--- daijie-springboot/daijie-jdbc-spring-boot-starter/src/main/java/org/daijie/jdbc/MapperClassPathBeanDefinitionScanner.java
+++ daijie-springboot/daijie-jdbc-spring-boot-starter/src/main/java/org/daijie/jdbc/MapperClassPathBeanDefinitionScanner.java
@@ -1,9 +1,9 @@
 package org.daijie.jdbc;
 
 import org.daijie.core.util.ClassLoaderUtil;
+import org.daijie.jdbc.annotation.Mapper;
 import org.daijie.jdbc.session.SessionMapper;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -29,31 +28,28 @@ public class MapperClassPathBeanDefinitionScanner implements ApplicationContextA
      * 扫描启动类包路径下的Mapper类，并注册到spring容器
      * @return bean实例集
      */
-    public Set<BeanDefinition> doScan() {
-        Set<BeanDefinition> beanDefinitions = new LinkedHashSet();
+    public Set<Class<?>> doScan() {
         Set<Class<?>> classes = null;
         if (ClassLoaderUtil.isRunTest()) {
             classes = ClassLoaderUtil.getClassesInPackage("");
         } else {
             classes = ClassLoaderUtil.getClassesInMianMethodClass();
         }
-        classes.forEach(cls -> {
-            Class<?>[] parentClass = cls.getInterfaces();
-            if (parentClass.length > 0 && parentClass[0].isAssignableFrom(SessionMapper.class)) {
+        return classes;
+    }
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
+        doScan().forEach(cls -> {
+            if (isMapperClass(cls)) {
                 BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(cls);
                 GenericBeanDefinition definition = (GenericBeanDefinition) builder.getRawBeanDefinition();
                 definition.getPropertyValues().add("interfaceClass", cls);
                 definition.setBeanClass(MapperFactoryBean.class);
                 definition.setAutowireMode(GenericBeanDefinition.AUTOWIRE_BY_TYPE);
-                beanDefinitions.add(definition);
+                beanDefinitionRegistry.registerBeanDefinition(cls.getSimpleName(), definition);
             }
         });
-        return beanDefinitions;
-    }
-
-    @Override
-    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
-        doScan().forEach(beanDefinition -> beanDefinitionRegistry.registerBeanDefinition(beanDefinition.getBeanClassName(), beanDefinition));
     }
 
     @Override
@@ -64,5 +60,21 @@ public class MapperClassPathBeanDefinitionScanner implements ApplicationContextA
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.context = applicationContext;
+    }
+
+    /**
+     * 判断类是不是声明为mapper类
+     * @param cls 指定类
+     * @return 是不是声明为mapper类
+     */
+    private boolean isMapperClass(Class<?> cls) {
+        Class<?>[] parentClass = cls.getInterfaces();
+        if (parentClass.length > 0 && parentClass[0].isAssignableFrom(SessionMapper.class)) {
+            return true;
+        }
+        if (cls.getAnnotation(Mapper.class) != null) {
+            return true;
+        }
+        return false;
     }
 }
